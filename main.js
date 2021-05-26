0903df82e876ee7345cd6f597e73e1c3a1132f33
chrram <chr.ram.94@gmail.com>
2021-05-26 17:20:34 +0200

    File handling

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow, Menu, MenuItem, ipcMain, dialog, net, clipboard, Tray } = require('electron')
-const {download} = require('electron-dl');
+const { download } = require('electron-dl');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -21,11 +21,11 @@ function createWindow() {
 ipcMain.on("upload", (event, arg) => {
 
   const options = {
-    title: 'Open a PDF file',
+    title: 'Open a JPG file',
     defaultPath: '/path/to/something/',
     buttonLabel: 'OK',
     filters: [
-      { name: 'pdf', extensions: ['pdf'] }
+      { name: 'jpg', extensions: ['jpg'] }
     ],
     properties: ['showHiddenFiles'],
     message: 'This message will only be shown on macOS'
@@ -33,7 +33,7 @@ ipcMain.on("upload", (event, arg) => {
 
   dialog.showOpenDialog(null, options
   ).then(result => {
-
+    console.log(result, "resss");
     return result.canceled || event.sender.send("upload_finished", result.filePaths)
 
   }).catch(err => {
@@ -84,34 +84,102 @@ ipcMain.on("upload", (event, arg) => {
 
 // });
 
-ipcMain.on("api_call", async (event, arg) => {
+ipcMain.on("api_call", async (event, filepath, angles, areas, heightCeiling, heightDetection) => {
+  const axios = require('axios');
+  const FormData = require("form-data")
 
-  const win = BrowserWindow.getFocusedWindow();
+  var formData = new FormData(filepath)
+  let fs = require ("fs")
+  fs.readFile(filepath, (err, data) =>{
+    if(err){console.log(err, "error handling fike");}
+    formData.append("floorplan",data ,filepath);
+  })
 
-  console.log(app.getPath('downloads'), arg,  "path");
-  await download(win, "http://httpbin.org/image/png",
-  {onCompleted: (info)=>  {event.sender.send("call_finished", info)},
-  filename: "optimized_" + arg.split(".")[0]+".png" })
+  console.log(filepath, angles, areas, heightCeiling, heightDetection, "args in the back");
   
+
+  axios.post('http://127.0.0.1:5000/plan/parse', formData, {
+    headers: {
+      'accept': 'application/json',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Content-Type': `multipart/form-data;`,
+    }
+  })
+    .then(res => {
+        console.log({res}, "success");
+    }).catch(err => {
+        console.error({err}, " rroeeererrror");
+        // console.error({err});
+    });
+  // axios({
+  //   method: 'post',
+  //   url: 'http://127.0.0.1:5000/plan/parse',
+  //   headers: {
+  //     'accept':
+  //     'Content-Type': 'multipart/form-data'
+  //   },
+  //   data: {
+  //     formData
+  //   }
+  // }).then((response) => {
+  //   console.log(response, " resonses");
+  // }, (error) => {
+  //   console.log(error, "error");
+  // });
+
+
+  // const request = net.request({
+  //   method: 'POST',
+  //   protocol: 'http:',
+  //   hostname: '127.0.0.1',
+  //   path: '/plan/parse',
+  //   redirect: 'follow',
+  //   // body: {"floorplan": filepath}
+  // });
+
+  // request.write({"test": "test"})
+
+  // request.on('response', (response) => {
+    
+  //   console.log(`STATUS: ${response.statusCode}`);
+  //   console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+  //   response.on('data', (chunk) => {
+  //     console.log(`BODY: ${chunk} eeeeeee`)
+  //   });
+
+  // });
+  // request.on('error', (error) => {
+  //   console.log(`ERROR: ${JSON.stringify(error)}`)
+  // });
+  // // request.write()s
+  // // request.setHeader('Content-Type', 'application/json');
+
+  // request.end();
+  // const win = BrowserWindow.getFocusedWindow();
+  // console.log(app.getPath('downloads'), arg,  "path");
+  // await download(win, "http://httpbin.org/image/png",
+  // {onCompleted: (info)=>  {event.sender.send("call_finished", info)},
+  // filename: "optimized_" + arg.split(".")[0]+".png" })
+
 });
 
 
-const menu = [
-  {
-    label: 'Files',
-    submenu: [
-      {
-        label: 'Add file',
-        click: async () => {
-          console.log("test");
-        }
-      },
-    ]
-  }
-]
+// const menu = [
+//   {
+//     label: 'Files',
+//     submenu: [
+//       {
+//         label: 'Add file',
+//         click: async () => {
+//           console.log("test");
+//         }
+//       },
+//     ]
+//   }
+// ]
 
-const builtmenu = Menu.buildFromTemplate(menu)
-Menu.setApplicationMenu(builtmenu)
+// const builtmenu = Menu.buildFromTemplate(menu)
+// Menu.setApplicationMenu(builtmenu)
 
 app.whenReady().then(createWindow)
 
