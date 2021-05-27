af89a15697da274acce17d2474bd60b5cd56220d
chrram <chr.ram.94@gmail.com>
2021-05-27 10:20:57 +0200

    The parsing is now done at the first page.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const { app, BrowserWindow, Menu, MenuItem, ipcMain, dialog, net, clipboard, Tray } = require('electron')
 const { download } = require('electron-dl');
 
@@ -84,109 +85,25 @@ ipcMain.on("upload", (event, arg) => {
 
 // });
 
-ipcMain.on("planParse", async (event, filepath, angles, areas, heightCeiling, heightDetection) => {
+ipcMain.on("optimize_call", async (event, filepath,  angles, areas, heightCeiling, heightDetection) => {
+
+  
+})
+
+ipcMain.on("planParse", async (event, filepath) => {
   const axios = require('axios');
   const FormData = require("form-data")
+  let fs = require ("fs")
 
   var formData = new FormData()
-  console.log(formData)
-  let fs = require ("fs")
-  try {
-    const file = fs.readFileSync(filepath)
-    console.log(typeof file)
-    formData.append("floorplan", file)
-    console.log(formData)
-  } catch (err) {
-    console.error(err)
-  }
-  // formData.append(file)
-  console.log(formData)
-
-
-  // console.log(filepath, angles, areas, heightCeiling, heightDetection, "args in the back");
-
-
-  axios.post('http://127.0.0.1:5000/plan/parse', formData, {
-    headers: {
-      'accept': 'application/json',
-      'Accept-Language': 'en-US,en;q=0.8',
-      'Content-Type': `multipart/form-data;`,
-    }
-  })
-    .then(res => {
-        console.log({res}, "success");
-    }).catch(err => {
-        pass
-        // console.error({err}, " rroeeererrror");
-        // console.error({err});
-    });
-  // axios({
-  //   method: 'post',
-  //   url: 'http://127.0.0.1:5000/plan/parse',
-  //   headers: {
-  //     'accept':
-  //     'Content-Type': 'multipart/form-data'
-  //   },
-  //   data: {
-  //     formData
-  //   }
-  // }).then((response) => {
-  //   console.log(response, " resonses");
-  // }, (error) => {
-  //   console.log(error, "error");
-  // });
-
-
-  // const request = net.request({
-  //   method: 'POST',
-  //   protocol: 'http:',
-  //   hostname: '127.0.0.1',
-  //   path: '/plan/parse',
-  //   redirect: 'follow',
-  //   // body: {"floorplan": filepath}
-  // });
-
-  // request.write({"test": "test"})
-
-  // request.on('response', (response) => {
-
-  //   console.log(`STATUS: ${response.statusCode}`);
-  //   console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-  //   response.on('data', (chunk) => {
-  //     console.log(`BODY: ${chunk} eeeeeee`)
-  //   });
-
-  // });
-  // request.on('error', (error) => {
-  //   console.log(`ERROR: ${JSON.stringify(error)}`)
-  // });
-  // // request.write()s
-  // // request.setHeader('Content-Type', 'application/json');
-
-  // request.end();
-  // const win = BrowserWindow.getFocusedWindow();
-  // console.log(app.getPath('downloads'), arg,  "path");
-  // await download(win, "http://httpbin.org/image/png",
-  // {onCompleted: (info)=>  {event.sender.send("call_finished", info)},
-  // filename: "optimized_" + arg.split(".")[0]+".png" })
-
-});
-
-
-ipcMain.on("api_call", async (event, filepath, angles, areas, heightCeiling, heightDetection) => {
-  const axios = require('axios');
-  const FormData = require("form-data")
-
-  var formData = new FormData(filepath)
-  let fs = require ("fs")
+ 
   fs.readFile(filepath, (err, data) =>{
     if(err){console.log(err, "error handling fike");}
+    // console.log(data,filepath);
     formData.append("floorplan",data ,filepath);
   })
 
-  console.log(filepath, angles, areas, heightCeiling, heightDetection, "args in the back");
-  
-
+  // console.log("formDatasss",formData, "tetst");
   axios.post('http://127.0.0.1:5000/plan/parse', formData, {
     headers: {
       'accept': 'application/json',
@@ -197,32 +114,13 @@ ipcMain.on("api_call", async (event, filepath, angles, areas, heightCeiling, hei
     .then(res => {
         console.log({res}, "success");
     }).catch(err => {
-        console.error({err}, " rroeeererrror");
-        // console.error({err});
+      // console.log(err, "error");
+        app.quit()
     });
   
 });
 
-
-// const menu = [
-//   {
-//     label: 'Files',
-//     submenu: [
-//       {
-//         label: 'Add file',
-//         click: async () => {
-//           console.log("test");
-//         }
-//       },
-//     ]
-//   }
-// ]
-
-// const builtmenu = Menu.buildFromTemplate(menu)
-// Menu.setApplicationMenu(builtmenu)
-
 app.whenReady().then(createWindow)
-
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
