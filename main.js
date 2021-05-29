e53b55777572be16c5ab6248ae6ebf0d11d46bfa
chrram <chr.ram.94@gmail.com>
2021-05-29 14:02:03 +0200

    It now draws the points on the map.

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ ipcMain.on("upload", (event, arg) => {
   })
 });
 
-ipcMain.on("optimize_call", async (event, sensor_field_of_view, areas, ceiling_height, detection_height) => {
+ipcMain.on("optimize_call", async (event,rooms, sensor_field_of_view, areas, ceiling_height, detection_height) => {
 
   const loading = new BrowserWindow({
     width: 1200,
@@ -73,6 +73,9 @@ ipcMain.on("optimize_call", async (event, sensor_field_of_view, areas, ceiling_h
   loading.show()
 
   let k = [[[70, 54], [70, 294], [208, 294], [217, 201], [218, 379], [300, 379], [301, 312], [496, 311], [496, 248], [302, 246], [495, 240], [496, 54]], [[505, 54], [504, 238], [613, 240], [615, 54]], [[621, 54], [624, 245], [753, 240], [755, 54]], [[498, 248], [498, 311], [668, 311], [670, 248]], [[681, 251], [681, 309], [753, 309], [753, 251]], [[73, 300], [73, 380], [208, 381], [208, 300]], [[506, 319], [507, 456], [755, 453], [753, 319]], [[351, 320], [354, 387], [385, 388], [385, 453], [498, 456], [496, 319]], [[311, 323], [311, 385], [340, 385], [340, 323]], [[73, 389], [73, 453], [136, 453], [136, 389]], [[151, 389], [151, 453], [208, 453], [208, 389]], [[218, 388], [218, 453], [300, 453], [300, 388]], [[311, 398], [311, 453], [375, 453], [375, 398]]]
+
+  console.log(k, "k ", rooms, "rooms test");
+
   const formData = new FormData();
   formData.append('rooms', JSON.stringify(k));
   formData.append('shape', JSON.stringify([542, 800]));
@@ -80,13 +83,19 @@ ipcMain.on("optimize_call", async (event, sensor_field_of_view, areas, ceiling_h
   formData.append('detection_height', 100);
   formData.append('sensors_field_of_view', 90);
 
+  // formData.append('rooms', JSON.stringify(rooms));
+  // formData.append('shape', JSON.stringify([542, 800]));
+  // formData.append('ceiling_height', ceiling_height;
+  // formData.append('detection_height', detection_height);
+  // formData.append('sensors_field_of_view', sensors_field_of_view);
+
+
   axios.post('http://127.0.0.1:5000/plan/optimize', formData, {
     headers: formData.getHeaders()
   })
     .then(response => {
       loading.hide()
       event.sender.send("optimizing_finished", response.data)
-      
     })
     .catch(error => {
       console.log(error, "error");
